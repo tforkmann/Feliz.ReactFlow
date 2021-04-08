ed50fe883c7476726f97462ed32baa8641047a09
Tim Forkmann <tforkmann@gmail.com>
2021-04-08 14:17:36 +0200

    get it working

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -54,34 +54,96 @@ let view (model: Model) (dispatch: Msg -> unit) =
                 ReactFlow.node [
                     node.id "1"
                     node.nodetype Input
-                    node.data {| label = "An Input Element with Custom Style" |}
+                    node.data {| label = "Erdgas Einsatz" |}
                     node.style [
-                      style.background "#D6D5E6"
-                      style.color  "#333"
+                      style.background "yellow"
+                      style.color  "#332"
+                      style.border "1px solid #222138"
+                      style.width 180
+                      ]
+                    node.position (20,5)
+                ]
+                ReactFlow.node [
+                    node.id "2"
+                    node.nodetype Default
+                    node.data {| label = "CityCube" |}
+                    node.style [
+                      style.background "#2e88c9"
+                      style.color "white"
                       style.border "1px solid #222138"
                       style.width 180
                       ]
                     node.position (250,5)
                 ]
                 ReactFlow.node [
-                    node.id "2"
+                    node.id "3"
                     node.nodetype Output
-                    node.data {| label = "An Output Element" |}
+                    node.data {| label = "Strom Absatz" |}
+                    node.style [
+                      style.background "lightblue"
+                      style.color  "#333"
+                      style.border "1px solid #222138"
+                      style.width 180
+                      ]
+                    node.position (100,100)
+                ]
+                ReactFlow.node [
+                    node.id "4"
+                    node.nodetype Output
+                    node.data {| label = "Wärme Absatz" |}
+                    node.style [
+                      style.background "red"
+                      style.color  "white"
+                      style.border "1px solid #222138"
+                      style.width 180
+                      ]
                     node.position (100,100)
                 ]
                 ReactFlow.edge [
                     edge.id "e1-2"
                     edge.source "1"
                     edge.target "2"
-                    edge.animated true
-                    edge.label "ConnectionLabel with Custom LableStyle"
-                    edge.edgeType Bezier
+                    edge.animated false
+                    edge.label "100 MWh"
+                    edge.edgeType SmoothStep
                     edge.arrowHeadType ArrowClosed
                     edge.style [
-                        style.stroke "#f6ab6c"
+                        style.stroke "yellow"
                     ]
                     edge.labelStyle [
-                        labelStyle.fill "#f6ab6c"
+                        labelStyle.fill "black"
+                        labelStyle.fontWeight 700
+                    ]
+                ]
+                ReactFlow.edge [
+                    edge.id "e2-3"
+                    edge.source "2"
+                    edge.target "3"
+                    edge.animated true
+                    edge.label "50 MWh"
+                    edge.edgeType SmoothStep
+                    edge.arrowHeadType ArrowClosed
+                    edge.style [
+                        style.stroke "blue"
+                    ]
+                    edge.labelStyle [
+                        labelStyle.fill "blue"
+                        labelStyle.fontWeight 700
+                    ]
+                ]
+                ReactFlow.edge [
+                    edge.id "e2-4"
+                    edge.source "2"
+                    edge.target "4"
+                    edge.animated true
+                    edge.label "55 MWh"
+                    edge.edgeType SmoothStep
+                    edge.arrowHeadType ArrowClosed
+                    edge.style [
+                        style.stroke "red"
+                    ]
+                    edge.labelStyle [
+                        labelStyle.fill "red"
                         labelStyle.fontWeight 700
                     ]
                 ]
--- src/Feliz.ReactFlow/Feliz.ReactFlow.fs
+++ src/Feliz.ReactFlow/Feliz.ReactFlow.fs
@@ -30,6 +30,9 @@ module Interop =
     let objectHas (keys: string list) (x: obj) =
         objectKeys (x) |> Seq.toList |> (=) keys
 
+    [<Emit("Object.assign({}, $0, $1)")>]
+    let objectAssign (x: obj) (y: obj) = jsNative
+
     let inline mkNodeAttr (key: string) (value: obj) : INodeProp = unbox (key, value)
 
 type position = { x: int; y: int }
@@ -53,59 +56,51 @@ type NodeType =
     | Default
     member this.Value = this.ToString().ToLower()
 
-
-
 [<Erase>]
 type style =
-    static member inline background (background:string)=
-        Interop.mkAttr "background" background
-    static member inline color (color:string)=
-        Interop.mkAttr "color" color
-    static member inline border (border:string)=
-        Interop.mkAttr "border" border
-    static member inline width(width: int) =
-        Interop.mkAttr "width" width
-    static member inline stroke(stroke: string) =
-        Interop.mkAttr "stroke" stroke
+    static member inline background(background: string) = Interop.mkAttr "background" background
+    static member inline color(color: string) = Interop.mkAttr "color" color
+    static member inline border(border: string) = Interop.mkAttr "border" border
+    static member inline width(width: int) = Interop.mkAttr "width" width
+    static member inline stroke(stroke: string) = Interop.mkAttr "stroke" stroke
 
 [<Erase>]
 type labelStyle =
-    static member inline fill (fill:string)=
-        Interop.mkAttr "fill" fill
-    static member inline fontWeight (fontWeight:int)=
-        Interop.mkAttr "fontWeight" fontWeight
+    static member inline fill(fill: string) = Interop.mkAttr "fill" fill
+    static member inline fontWeight(fontWeight: int) = Interop.mkAttr "fontWeight" fontWeight
 
 [<Erase>]
 type node =
-    static member inline position(x: int,y: int) =
-        Interop.mkAttr "position" [| x; y |]
+    static member inline position(x: int, y: int) =
+        Interop.mkNodeAttr "position" {| x = x; y = y |}
+
     static member inline nodetype(nodeType: NodeType) =
-        Interop.mkAttr "type" nodeType.Value
-    static member inline data(data: obj) =
-        Interop.mkAttr "data" data
+        Interop.mkNodeAttr "type" nodeType.Value
+
+    static member inline data(data: obj) = Interop.mkNodeAttr "data" data
+
     static member inline style props =
-        Interop.mkAttr "style" (createObj !!props)
-    static member inline id(id: string) =
-        Interop.mkAttr "id" id
+        Interop.mkNodeAttr "style" (createObj !!props)
+
+    static member inline id(id: string) = Interop.mkNodeAttr "id" id
 
 [<Erase>]
 type edge =
-    static member inline source(source:string) =
-        Interop.mkAttr "source" source
-    static member inline target(target: string) =
-        Interop.mkAttr "target" target
-    static member inline animated(animated:bool) =
-        Interop.mkAttr "animated" animated
-    static member inline arrowHeadType(arrowHeadType:ArrowHead) =
+    static member inline source(source: string) = Interop.mkAttr "source" source
+    static member inline target(target: string) = Interop.mkAttr "target" target
+    static member inline animated(animated: bool) = Interop.mkAttr "animated" animated
+
+    static member inline arrowHeadType(arrowHeadType: ArrowHead) =
         Interop.mkAttr "arrowHeadType" arrowHeadType.Value
-    static member inline label(label: obj) =
-        Interop.mkAttr "label" label
-    static member inline edgeType(edgeType: EdgeType) =
-        Interop.mkAttr "type" edgeType.Value
+
+    static member inline label(label: obj) = Interop.mkAttr "label" label
+    static member inline edgeType(edgeType: EdgeType) = Interop.mkAttr "type" edgeType.Value
+
     static member inline labelStyle props =
         Interop.mkAttr "labelStyle" (createObj !!props)
-    static member inline id(id: string) =
-        Interop.mkAttr "id" id
+
+    static member inline id(id: string) = Interop.mkAttr "id" id
+
     static member inline style props =
         Interop.mkAttr "style" (createObj !!props)
 
@@ -118,20 +113,14 @@ type ReactFlow =
     /// Creates a new ReactFlow component.
     static member inline flowChart(props: IReactFlowProp seq) =
         Interop.reactApi.createElement (reactFlow, createObj !!props)
-    static member inline node props =
-        Interop.mkAttr "node" (createObj !!props)
-    static member inline edge props =
-        Interop.mkAttr "edge" (createObj !!props)
 
-        // ("node" ==> props)
-        // "node" ==> (unbox props)
-        // prop.custom("node",props)
-        // printfn "props %A" props
-        // printfn "obj %A" (createObj !!props)
-        // Interop.reactApi.createElement (reactFlow, createObj !!props)
+    static member inline node props =
+        Interop.objectAssign obj (createObj !!props)
+
+    static member inline edge props =
+        Interop.objectAssign obj (createObj !!props)
     /// Provides the child elements in a flow.
-    static member inline elements(elements: _ array) : IReactFlowProp =
-        !!("elements" ==> elements)
+    static member inline elements(elements: obj array) : IReactFlowProp = !!("elements" ==> elements)
 
     static member inline onElementClick(handler: (obj * obj) -> unit) : IReactFlowProp =
         !!("onConnectStart" ==> funcToTuple handler)
